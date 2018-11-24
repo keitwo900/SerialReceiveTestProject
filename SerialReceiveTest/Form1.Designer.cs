43e7c94e7f908c7bfb6af1fa2725ac86884f10e6
kkasuga <kkasuga900@gmail.com>
2018-11-24 17:53:14 +0900

    ・Baudrate変更
    ■主な変更点
    ・SerialPortのBaudrateを115200bpsに変更しました。
    
    ■変更ファイル
    SerialReceiveTest/Form1.Designer.cs
    ・SerialPortのBaudrateを115200bpsに変更しました。

--- SerialReceiveTest/Form1.Designer.cs
+++ SerialReceiveTest/Form1.Designer.cs
@@ -102,6 +102,7 @@
 			// 
 			// serialPort1
 			// 
+			this.serialPort1.BaudRate = 115200;
 			this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
 			// 
 			// button_MsgClear
